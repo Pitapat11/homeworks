fbdbf9b4deed9a01941b7e155ec44a6a10874e6f
Данил <kasyanchukdaniil@gmail.com>
2025-03-01 02:35:16 +0200

    Homework number 2

--- homework#2.py
+++ homework#2.py
@@ -1,11 +1,11 @@
 num = int(input("enter 5 digit number: "))
 
-n1 = num // 10000
-n2 = (num % 10000) // 1000
-n3 = (num % 1000) // 100
-n4 = (num % 100) // 10
-n5 = num % 10
+ten_ths = num // 10000
+thous = (num % 10000) // 1000
+hunds = (num % 1000) // 100
+tens = (num % 100) // 10
+ones = num % 10
 
-result = n5 * 10000 + n4 * 1000 + n3 * 100 + n2 * 10 + n1
+result = ones * 10000 + tens * 1000 + hunds * 100 + thous * 10 + ten_ths
 
 print(result)
