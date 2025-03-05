b2a427920ce10e1c6eaafee05c173e2bed1e1188
Данил <kasyanchukdaniil@gmail.com>
2025-03-06 01:28:52 +0200

    Homework number 3

---
+++ homework#3.py
@@ -0,0 +1,43 @@
+#v1
+#first_digit = int(input("enter the first digit: "))
+#second_digit=int(input("enter the second digit: "))
+#operation=(input("select operation ( + ,- ,/ ,* ): "))
+
+#match operation:
+#    case "+":
+#        print("Result: ", first_digit + second_digit)
+#    case "-":
+#        print("Result: ", first_digit - second_digit)
+#    case "*":
+#        print("Result: ", first_digit * second_digit)
+#    case "/":
+#            if second_digit != 0:
+#                print("Result:", first_digit / second_digit)
+#            else:
+#                print("Error: division by zero")
+#    case _:
+#          print("Invalid operation")
+
+#v2
+first_digit = int(input("enter the first digit: "))
+second_digit = int(input("enter the second digit: "))
+operation = input("select operation (+, -, /, *): ")
+
+
+match (operation, second_digit):
+    case ("+",_):
+        print("Result:", first_digit + second_digit)
+    case ("-",_):
+        print("Result:", first_digit - second_digit)
+    case ("*",_):
+        print("Result:", first_digit * second_digit)
+    case ("/", 0):
+        print("Error: Division by zero")
+    case ("/",_):
+        print("Result:", first_digit / second_digit)
+    case _:
+        print("Invalid operation")
+
+
+
+
