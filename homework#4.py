8fae4cf27dbdcb7ebfc860220708f17f930523bb
Данил <kasyanchukdaniil@gmail.com>
2025-03-08 00:49:12 +0200

    Homework number 3

---
+++ homework#4.py
@@ -0,0 +1,11 @@
+nums = [1,2,3]
+middle_indexlist = len(nums) // 2
+
+if len(nums) % 2 != 0:
+    middle_indexlist += 1
+
+half1 = nums[:middle_indexlist]
+half2 = nums[middle_indexlist:]
+
+result = [half1, half2]
+print(result)
