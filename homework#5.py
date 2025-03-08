97b448d7c134660f4e975ef1696f72b20ec71f83
Данил <kasyanchukdaniil@gmail.com>
2025-03-08 02:50:28 +0200

    Homework number 5

---
+++ homework#5.py
@@ -0,0 +1,15 @@
+# v1
+# nums = []
+# if len(nums) > 0:
+#     nums.insert(0, nums[-1])
+#     nums.pop()
+#
+# print(nums)
+
+# v2
+
+# nums=[12,3,4,10]
+#
+# if len(nums)> 0:
+#     nums= [nums[-1]] + nums [:-1]
+# print(nums)
