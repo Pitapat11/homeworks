08b7eb80a3197d6da7c80c9bfe9a58af33bca44b
Данил <kasyanchukdaniil@gmail.com>
2025-03-11 01:58:49 +0200

    Homework number 8

--- homework#8.py
+++ homework#8.py
@@ -1,4 +1,4 @@
-nums= []
+nums= [1,3,4]
 even_nums=0
 if not nums:
     print(0)
@@ -6,4 +6,4 @@ else:
     for i in range(0, len(nums), 2):
         even_nums+=nums[i]*nums[-1]
         # print(nums[i], end=' ')
-    print(even_nums)
+    print("Result:",even_nums)
