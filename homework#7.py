a579678008af794839010ad891a1b6a8f7d07180
Данил <kasyanchukdaniil@gmail.com>
2025-03-11 02:00:23 +0200

    Homework number 7

--- homework#7.py
+++ homework#7.py
@@ -1,10 +1,10 @@
 import random
 
-
 length = random.randint(3, 10)
 
 random_list = [random.randint(1, 100) for _ in range(length)]
 print("Default list:",random_list)
+
 if length >= 3:
     new_list = [random_list[0], random_list[2], random_list[-2]]
     print("New list:", new_list)
