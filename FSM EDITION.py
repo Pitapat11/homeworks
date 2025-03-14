576ff0cd52d420be22416e6b81d1a8a0306c2ae7
Данил <kasyanchukdaniil@gmail.com>
2025-03-14 17:30:21 +0200

    FSM EDITION

---
+++ FSM EDITION.py
@@ -0,0 +1,410 @@
+# from pynput.mouse import Listener
+#
+# def on_click(x, y, button, pressed):
+#     if pressed:
+#         print(f"Координаты: X={x}, Y={y}")
+#
+# with Listener(on_click=on_click) as listener:
+#     listener.join()
+# #
+
+# from fileinput import close
+
+
+
+
+# import pyautogui
+# import time
+#
+#
+# y_scroll= 401
+# step= 4
+#
+# account_counter = 0
+#
+#
+# first_acc = (1545, 394)
+# second_acc= (1544, 419)
+# third_acc =(1545, 445)
+# firth_acc =(1545, 466)
+# fifth_acc =(1548, 492)
+#
+#
+#
+#
+#
+# click_panel=(1688, 432)
+# start_acc = (1740, 398)
+# close_acc = (1742, 442)
+# open_pubg = (1780, 647)
+#
+# while True:
+#     for acc in [first_acc, second_acc, third_acc, firth_acc, fifth_acc]:
+#
+#         pyautogui.click(first_acc)
+#         print(f"Нажали на first acc")
+#         time.sleep(2)
+#
+#         pyautogui.click(start_acc)
+#         print(f'Нажал на start acc')
+#         time.sleep(30)
+#
+#         pyautogui.doubleClick(open_pubg)
+#         print(f"Запустил pubg")
+#         time.sleep(90)
+#
+#         pyautogui.doubleClick(click_panel)
+#         print(f"Клик по панели")
+#         time.sleep(3)
+#
+#         pyautogui.click(first_acc)
+#         print(f"Нажали на first_acc")
+#         time.sleep(3)
+#
+#         pyautogui.click(close_acc)
+#         print(f"Нажали на close acc")
+#         time.sleep(5)
+#
+#         pyautogui.click(second_acc)
+#         print(f"Нажали на second acc")
+#         time.sleep(1)
+#
+#         pyautogui.click(start_acc)
+#         print(f'Нажал на start acc')
+#         time.sleep(70)
+#
+#         pyautogui.doubleClick(open_pubg)
+#         print(f"Запустил pubg")
+#         time.sleep(90)
+#
+#         pyautogui.doubleClick(click_panel)
+#         print(f"Клик по панели")
+#         time.sleep(3)
+#
+#         pyautogui.click(second_acc)
+#         print(f"Нажали на second_acc")
+#         time.sleep(3)
+#
+#         pyautogui.click(close_acc)
+#         print(f"Нажали на close acc")
+#         time.sleep(5)
+#
+#         pyautogui.click(third_acc)
+#         print(f"Нажали на third acc")
+#         time.sleep(1)
+#
+#         pyautogui.click(start_acc)
+#         print(f'Нажал на start acc')
+#         time.sleep(70)
+#
+#         pyautogui.doubleClick(open_pubg)
+#         print(f"Запустил pubg")
+#         time.sleep(90)
+#
+#         pyautogui.doubleClick(click_panel)
+#         print(f"Клик по панели")
+#         time.sleep(3)
+#
+#         pyautogui.click(third_acc)
+#         print(f"Нажали на third_acc")
+#         time.sleep(3)
+#
+#         pyautogui.click(close_acc)
+#         print(f"Нажали на close acc")
+#         time.sleep(5)
+#
+#         pyautogui.click(firth_acc)
+#         print(f"Нажали на firth acc")
+#         time.sleep(1)
+#
+#         pyautogui.click(start_acc)
+#         print(f'Нажал на start acc')
+#         time.sleep(70)
+#
+#         pyautogui.doubleClick(open_pubg)
+#         print(f"Запустил pubg")
+#         time.sleep(90)
+#
+#         pyautogui.doubleClick(click_panel)
+#         print(f"Клик по панели")
+#         time.sleep(3)
+#
+#         pyautogui.click(firth_acc)
+#         print(f"Нажали на firth_acc")
+#         time.sleep(3)
+#
+#         pyautogui.click(close_acc)
+#         print(f"Нажали на stop_coor")
+#         time.sleep(5)
+#
+#         pyautogui.click(fifth_acc)
+#         print(f"Нажали на fifth acc")
+#         time.sleep(3)
+#
+#         pyautogui.click(start_acc)
+#         print(f'Нажал на start acc')
+#         time.sleep(70)
+#
+#         pyautogui.doubleClick(open_pubg)
+#         print(f"Запустил pubg")
+#         time.sleep(90)
+#
+#         pyautogui.doubleClick(click_panel)
+#         print(f"Клик по панели")
+#         time.sleep(3)
+#
+#         pyautogui.click(fifth_acc)
+#         print(f"Нажали на fifth_acc")
+#         time.sleep(1)
+#
+#         pyautogui.click(close_acc)
+#         print(f"Нажали на close acc")
+#         time.sleep(5)
+#
+#         account_counter += 1
+#
+#         print(account_counter)
+#         if account_counter % 1 == 0:
+#             pyautogui.click(1623, y_scroll)
+#             pyautogui.scroll(-4900)
+#             print(f"Нажал на корды что бы листнуть")
+#             y_scroll+=step
+#             time.sleep(5)
+
+
+#WITH PAUSE
+
+import pyautogui
+import time
+import keyboard
+import threading
+
+y_scroll= 401
+step= 4
+
+account_counter = 0
+
+first_acc = (1545, 394)
+second_acc= (1544, 419)
+third_acc =(1545, 445)
+firth_acc =(1545, 466)
+fifth_acc =(1548, 492)
+click_panel=(1688, 432)
+start_acc = (1740, 398)
+close_acc = (1742, 442)
+open_pubg = (1780, 647)
+
+
+paused = False
+
+def pause_listener():
+    global paused
+    while True:
+        if keyboard.is_pressed('p'):
+            paused = True
+            print("Пауза... Нажмите 'r' для продолжения")
+            while paused:
+                if keyboard.is_pressed('r'):
+                    paused = False
+                    print("Продолжаем...")
+                    break
+        time.sleep(0.1)
+
+
+threading.Thread(target=pause_listener, daemon=True).start()
+
+
+while True:
+    for acc in [first_acc, second_acc, third_acc, firth_acc, fifth_acc]:
+        while paused:
+            time.sleep(0.1)
+
+        pyautogui.click(first_acc)
+        print(f"Нажали на first acc")
+        time.sleep(2)
+
+        while paused:
+            time.sleep(0.1)
+        pyautogui.click(start_acc)
+        print(f'Нажал на start acc')
+        time.sleep(70)
+
+        while paused:
+            time.sleep(0.1)
+        pyautogui.doubleClick(open_pubg)
+        print(f"Запустил pubg")
+        time.sleep(90)
+
+        while paused:
+            time.sleep(0.1)
+        pyautogui.doubleClick(click_panel)
+        print(f"Клик по панели")
+        time.sleep(2)
+
+        while paused:
+            time.sleep(0.1)
+        pyautogui.click(first_acc)
+        print(f"Нажали на first_acc")
+        time.sleep(3)
+
+        while paused:
+            time.sleep(0.1)
+        pyautogui.click(close_acc)
+        print(f"Нажали на close acc")
+        time.sleep(5)
+
+        while paused:
+            time.sleep(0.1)
+        pyautogui.click(second_acc)
+        print(f"Нажали на second acc")
+        time.sleep(1)
+
+        while paused:
+            time.sleep(0.1)
+        pyautogui.click(start_acc)
+        print(f'Нажал на start acc')
+        time.sleep(70)
+
+        while paused:
+            time.sleep(0.1)
+        pyautogui.doubleClick(open_pubg)
+        print(f"Запустил pubg")
+        time.sleep(90)
+
+        while paused:
+            time.sleep(0.1)
+        pyautogui.doubleClick(click_panel)
+        print(f"Клик по панели")
+        time.sleep(3)
+
+        while paused:
+            time.sleep(0.1)
+        pyautogui.click(second_acc)
+        print(f"Нажали на second_acc")
+        time.sleep(3)
+
+        while paused:
+            time.sleep(0.1)
+        pyautogui.click(close_acc)
+        print(f"Нажали на close acc")
+        time.sleep(5)
+
+        while paused:
+            time.sleep(0.1)
+        pyautogui.click(third_acc)
+        print(f"Нажали на third acc")
+        time.sleep(1)
+
+        while paused:
+            time.sleep(0.1)
+        pyautogui.click(start_acc)
+        print(f'Нажал на start acc')
+        time.sleep(70)
+
+        while paused:
+            time.sleep(0.1)
+        pyautogui.doubleClick(open_pubg)
+        print(f"Запустил pubg")
+        time.sleep(90)
+
+        while paused:
+            time.sleep(0.1)
+        pyautogui.doubleClick(click_panel)
+        print(f"Клик по панели")
+        time.sleep(3)
+
+        while paused:
+            time.sleep(0.1)
+        pyautogui.click(third_acc)
+        print(f"Нажали на third_acc")
+        time.sleep(3)
+
+        while paused:
+            time.sleep(0.1)
+        pyautogui.click(close_acc)
+        print(f"Нажали на close acc")
+        time.sleep(5)
+
+        while paused:
+            time.sleep(0.1)
+        pyautogui.click(firth_acc)
+        print(f"Нажали на firth acc")
+        time.sleep(1)
+
+        while paused:
+            time.sleep(0.1)
+        pyautogui.click(start_acc)
+        print(f'Нажал на start acc')
+        time.sleep(70)
+
+        while paused:
+            time.sleep(0.1)
+        pyautogui.doubleClick(open_pubg)
+        print(f"Запустил pubg")
+        time.sleep(90)
+
+        while paused:
+            time.sleep(0.1)
+        pyautogui.doubleClick(click_panel)
+        print(f"Клик по панели")
+        time.sleep(3)
+
+        while paused:
+            time.sleep(0.1)
+        pyautogui.click(firth_acc)
+        print(f"Нажали на firth_acc")
+        time.sleep(3)
+
+        while paused:
+            time.sleep(0.1)
+        pyautogui.click(close_acc)
+        print(f"Нажали на stop_coor")
+        time.sleep(5)
+
+        while paused:
+            time.sleep(0.1)
+        pyautogui.click(fifth_acc)
+        print(f"Нажали на fifth acc")
+        time.sleep(3)
+
+        while paused:
+            time.sleep(0.1)
+        pyautogui.click(start_acc)
+        print(f'Нажал на start acc')
+        time.sleep(70)
+
+        while paused:
+            time.sleep(0.1)
+        pyautogui.doubleClick(open_pubg)
+        print(f"Запустил pubg")
+        time.sleep(90)
+
+        while paused:
+            time.sleep(0.1)
+        pyautogui.doubleClick(click_panel)
+        print(f"Клик по панели")
+        time.sleep(3)
+
+        while paused:
+            time.sleep(0.1)
+        pyautogui.click(fifth_acc)
+        print(f"Нажали на fifth_acc")
+        time.sleep(1)
+
+        while paused:
+            time.sleep(0.1)
+        pyautogui.click(close_acc)
+        print(f"Нажали на close acc")
+        time.sleep(5)
+
+        account_counter += 1
+        print(account_counter)
+
+        while paused:
+            time.sleep(0.1)
+        if account_counter % 1 == 0:
+            pyautogui.click(1623, y_scroll)
+            pyautogui.scroll(-4900)
+            print(f"Нажал на корды что бы листнуть")
+            y_scroll+=step
+            time.sleep(5)
--- test.py
+++ test.py
@@ -14,168 +14,156 @@
 
 import pyautogui
 import time
-
-account_counter = 0
-
-tri_tochki1=(1796,119)
-tri_tochki2=(1796,155)
-tri_tochki3=(1796,193)
-tri_tochki4=(1796,234)
-tri_tochki5=(1796,269)
-
-
-
-first_acc = (1647,115)
-second_acc= (1647,154)
-third_acc =(1647,195)
-firth_acc =(1647,234)
-fifth_acc =(1647,273)
-
-
-
-open_steam1 = (1681, 231)
-open_steam2 = (1708, 271)
-open_steam3 = (1683, 309)
-open_steam4 = (1687, 340)
-open_steam5 = (1694, 386)
-
-
-click_panel = (1897, 16)
-open_pubg = (1858,442)
-nextpg_coor = (1855, 70)
-stop_coor = (1895, 313)
-
-while True:
-    for acc in [first_acc, second_acc, third_acc, firth_acc, fifth_acc]:
-
-        pyautogui.click(tri_tochki1)
-        print(f"Нажали на Три точки")
-        time.sleep(3)
-
-        pyautogui.click(open_steam1)
-        print(f'Нажал на open_steam')
-        time.sleep(90)
-
-        pyautogui.doubleClick(open_pubg)
-        print(f"Запустил pubg")
-        time.sleep(120)
-
-        pyautogui.doubleClick(click_panel)
-        print(f"Клик по панели")
-        time.sleep(3)
-
-        pyautogui.click(first_acc)
-        print(f"Нажали на first_acc")
-        time.sleep(3)
-
-        pyautogui.click(stop_coor)
-        print(f"Нажали на stop_coor")
-        time.sleep(5)
-
-        pyautogui.click(tri_tochki2)
-        print(f"Нажали на Три точки")
-        time.sleep(3)
-
-        pyautogui.click(open_steam2)
-        print(f'Нажал на open_steam')
-        time.sleep(90)
-
-        pyautogui.doubleClick(open_pubg)
-        print(f"Запустил pubg")
-        time.sleep(120)
-
-        pyautogui.doubleClick(click_panel)
-        print(f"Клик по панели")
-        time.sleep(3)
-
-        pyautogui.click(second_acc)
-        print(f"Нажали на second_acc")
-        time.sleep(3)
-
-        pyautogui.click(stop_coor)
-        print(f"Нажали на stop_coor")
-        time.sleep(5)
-
-        pyautogui.click(tri_tochki3)
-        print(f"Нажали на три точки")
-        time.sleep(3)
-
-        pyautogui.click(open_steam3)
-        print(f'Нажал на open_steam')
-        time.sleep(90)
-
-        pyautogui.doubleClick(open_pubg)
-        print(f"Запустил pubg")
-        time.sleep(120)
-
-        pyautogui.doubleClick(click_panel)
-        print(f"Клик по панели")
-        time.sleep(3)
-
-        pyautogui.click(third_acc)
-        print(f"Нажали на third_acc")
-        time.sleep(3)
-
-        pyautogui.click(stop_coor)
-        print(f"Нажали на stop_coor")
-        time.sleep(5)
-
-        pyautogui.click(tri_tochki4)
-        print(f"Нажали на Три точки")
-        time.sleep(3)
-
-        pyautogui.click(open_steam4)
-        print(f'Нажал на open_steam')
-        time.sleep(90)
-
-        pyautogui.doubleClick(open_pubg)
-        print(f"Запустил pubg")
-        time.sleep(120)
-
-        pyautogui.doubleClick(click_panel)
-        print(f"Клик по панели")
-        time.sleep(3)
-
-        pyautogui.click(firth_acc)
-        print(f"Нажали на firth_acc")
-        time.sleep(3)
-
-        pyautogui.click(stop_coor)
-        print(f"Нажали на stop_coor")
-        time.sleep(5)
-
-        pyautogui.click(tri_tochki5)
-        print(f"Нажали на Три точки")
-        time.sleep(3)
-
-        pyautogui.click(open_steam5)
-        print(f'Нажал на open_steam')
-        time.sleep(90)
-
-        pyautogui.doubleClick(open_pubg)
-        print(f"Запустил pubg")
-        time.sleep(120)
-
-        pyautogui.doubleClick(click_panel)
-        print(f"Клик по панели")
-        time.sleep(3)
-
-        pyautogui.click(fifth_acc)
-        print(f"Нажали на fifth_acc")
-        time.sleep(3)
-
-        pyautogui.click(stop_coor)
-        print(f"Нажали на stop_coor")
-        time.sleep(5)
-
-        account_counter += 1
-
-        print(account_counter)
-        if account_counter % 1 == 0:
-            pyautogui.click(nextpg_coor)
-            print(f"Нажали на координаты для смены страницы: nextpg_coor")
-            time.sleep(10)
-
+#
+# account_counter = 0
+#
+#
+# first_acc = (1647,115)
+# second_acc= (1647,154)
+# third_acc =(1647,195)
+# firth_acc =(1647,234)
+# fifth_acc =(1647,273)
+#
+#
+#
+# start_sel_acc = (1681, 231)
+# close_sel_acc = (1895, 313)
+# click_panel = (1897, 16)
+# open_pubg = (1858,442)
+# nextpg_coor = (1855, 70)
+# scroll_coor = (1,1)
+#
+#
+# while True:
+#     for acc in [first_acc, second_acc, third_acc, firth_acc, fifth_acc]:
+#
+#         pyautogui.click(first_acc)
+#         print(f"Нажали на first acc")
+#         time.sleep(3)
+#
+#         pyautogui.click(start_sel_acc)
+#         print(f'Нажал на запуск первого аккаунта')
+#         time.sleep(90)
+#
+#         pyautogui.doubleClick(open_pubg)
+#         print(f"Запустил pubg")
+#         time.sleep(120)
+#
+#         pyautogui.click(click_panel)
+#         print(f"Клик по панели")
+#         time.sleep(3)
+#
+#         pyautogui.click(first_acc)
+#         print(f"Нажали на первый акк")
+#         time.sleep(3)
+#
+#         pyautogui.click(close_sel_acc)
+#         print(f"Нажали на закрытие первого акка")
+#         time.sleep(5)
+#
+#         pyautogui.click(second_acc)
+#         print(f"Нажали на второй аккаунт")
+#         time.sleep(3)
+#
+#         pyautogui.click(start_sel_acc)
+#         print(f'Нажал на запуск аккаунта')
+#         time.sleep(90)
+#
+#         pyautogui.doubleClick(open_pubg)
+#         print(f"Запустил pubg")
+#         time.sleep(120)
+#
+#         pyautogui.doubleClick(click_panel)
+#         print(f"Клик по панели")
+#         time.sleep(3)
+#
+#         pyautogui.click(second_acc)
+#         print(f"Нажали на second_acc")
+#         time.sleep(3)
+#
+#         pyautogui.click(close_sel_acc)
+#         print(f"Нажали на закрытие второго акка")
+#         time.sleep(5)
+#
+#         pyautogui.click(third_acc)
+#         print(f"Нажали на третий аккаунт")
+#         time.sleep(3)
+#
+#         pyautogui.click(start_sel_acc)
+#         print(f'Нажал на запуск аккаунта')
+#         time.sleep(90)
+#
+#         pyautogui.doubleClick(open_pubg)
+#         print(f"Запустил pubg")
+#         time.sleep(120)
+#
+#         pyautogui.doubleClick(click_panel)
+#         print(f"Клик по панели")
+#         time.sleep(3)
+#
+#         pyautogui.click(third_acc)
+#         print(f"Нажали на третий акк")
+#         time.sleep(3)
+#
+#         pyautogui.click(close_sel_acc)
+#         print(f"Нажали на закрытие акка")
+#         time.sleep(5)
+#
+#         pyautogui.click(firth_acc)
+#         print(f"Нажали на четвертый акк")
+#         time.sleep(3)
+#
+#         pyautogui.click(start_sel_acc)
+#         print(f'Нажал на запуск аккаунта')
+#         time.sleep(90)
+#
+#         pyautogui.doubleClick(open_pubg)
+#         print(f"Запустил pubg")
+#         time.sleep(120)
+#
+#         pyautogui.doubleClick(click_panel)
+#         print(f"Клик по панели")
+#         time.sleep(3)
+#
+#         pyautogui.click(firth_acc)
+#         print(f"Нажали на четвертый аккаунт")
+#         time.sleep(3)
+#
+#         pyautogui.click(close_sel_acc)
+#         print(f"Нажали на закрытие аккаунта")
+#         time.sleep(5)
+#
+#         pyautogui.click(fifth_acc)
+#         print(f"Нажали на пятый аккаунт")
+#         time.sleep(3)
+#
+#         pyautogui.click(start_sel_acc)
+#         print(f'Нажал на открытие аккаунта')
+#         time.sleep(90)
+#
+#         pyautogui.doubleClick(open_pubg)
+#         print(f"Запустил pubg")
+#         time.sleep(120)
+#
+#         pyautogui.doubleClick(click_panel)
+#         print(f"Клик по панели")
+#         time.sleep(3)
+#
+#         pyautogui.click(fifth_acc)
+#         print(f"Нажали на fifth_acc")
+#         time.sleep(3)
+#
+#         pyautogui.click(close_sel_acc)
+#         print(f"Нажали на закрытие")
+#         time.sleep(5)
+#
+#         account_counter += 1
+#
+#         print(account_counter)
+#         if account_counter % 1 == 0:
+#             pyautogui.click(nextpg_coor)
+#             print(f"Нажали на координаты для смены страницы: nextpg_coor")
+#             time.sleep(10)
 
 
 
